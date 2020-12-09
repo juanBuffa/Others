f2d4e262332098ef3dca25e288827f359b68aca3
JuanMarcoBuffa <54648121+juanBuffa@users.noreply.github.com>
2020-12-09 16:11:44 -0300

    Update ServicioMailFeedback.java

--- futbolApp/src/main/java/com/juanmabu/futbolApp/servicios/ServicioMailFeedback.java
+++ futbolApp/src/main/java/com/juanmabu/futbolApp/servicios/ServicioMailFeedback.java
@@ -1,11 +1,12 @@
 package com.juanmabu.futbolApp.servicios;
 
-import com.sun.mail.smtp.SMTPTransport;
 import java.util.Date;
 import java.util.Properties;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
+import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import org.springframework.stereotype.Service;
@@ -22,34 +23,34 @@ public class ServicioMailFeedback {
         Properties prop = System.getProperties();
         prop.put("mail.smtp.host", "smtp.gmail.com");
         prop.put("mail.smtp.auth", "true");
-        prop.put("mail.smtp.port", "587");
-        prop.put("mail.smtp.starttls.enable", "true");
+        prop.put("mail.smtp.port", "465");
+        prop.put("mail.smtp.ssl.enable", "true");
 
-        Session session = Session.getInstance(prop, null);
+        Session session = Session.getInstance(prop, new javax.mail.Authenticator() {
+
+            protected PasswordAuthentication getPasswordAuthentication() {
+
+                return new PasswordAuthentication("cuentaparaprobarjava@gmail.com", System.getenv("futbol_pass"));
+
+            }
+
+        });
         Message msg = new MimeMessage(session);
-
+        session.setDebug(true);
         try {
 
             msg.setFrom(new InternetAddress("cuentaparaprobarjava@gmail.com"));
 
             msg.setRecipients(Message.RecipientType.TO,
                     InternetAddress.parse("cuentaparaprobarjava@gmail.com", false));
-            
+
             msg.setSubject("Feedback sobre la pagina futbolApp");
 
             msg.setText("Opinion sobre futbolAPP: " + feedback);
 
             msg.setSentDate(new Date());
 
-            SMTPTransport t = (SMTPTransport) session.getTransport("smtp");
-            
-            t.connect("smtp.gmail.com", "cuentaparaprobarjava@gmail.com", System.getenv("futbol_pass"));
-
-            t.sendMessage(msg, msg.getAllRecipients());
-
-            System.out.println("Response: " + t.getLastServerResponse());
-
-            t.close();
+            Transport.send(msg);
 
         } catch (MessagingException e) {
             e.printStackTrace();
